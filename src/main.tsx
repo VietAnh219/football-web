e69d7c4f020d3cec49c753537061a68368c7a0cc
VietAnh219 <phungvietanh21092003@gmail.com>
2025-05-08 00:19:40 +0700

    add vercel.json to fix routing issue

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PayPalScriptProvider } from "@paypal/react-paypal-js"
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
 
@@ -12,9 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <PayPalScriptProvider options={{ "clientId": import.meta.env.VITE_PAYPAL }}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <App />
       </PayPalScriptProvider>
     </QueryClientProvider>
   </StrictMode>,
